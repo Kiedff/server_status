c99779f20971b96919e43ca63703c91e86f94131
Kiedff <kiedff7@gmail.com>
2016-07-28 15:48:04 +0100

    basic new, create, show actions/views for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,29 @@
 class EventsController < ApplicationController
+
+  def new
+    @statuses = Status.all
+    @event = Event.new
+  end
+
+  def create
+    @status = Status.find(params[:status_id])
+    @event = @status.events.new(event_params)
+
+    if @event.save
+      flash[:notice] = "Event successfully created"
+    else
+      flash[:error] = "Unable to create event"
+    end
+    redirect_to root_path
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :content, :active_at, :status_id, :resolved_at, :aasm_state)
+  end
 end
