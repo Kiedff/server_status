268238b83ff107bc8efabf7f782636f6060bf037
Kiedff <kiedff7@gmail.com>
2016-07-28 18:02:28 +0100

    adds new/create actions to comments, updates comments layout events show page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,26 @@
 class CommentsController < ApplicationController
+
+  def new
+    @event = Event.find(params[:event_id])
+    @comment = Comment.new
+  end
+
+  def create
+    
+    event = Event.find(params[:event_id])
+    comment = event.comments.new(comment_params)
+
+    if comment.save
+      flash[:notice] = "Comment successfully created"
+    else
+      flash[:error] = "Unable to create comment"
+    end
+    redirect_to event
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body, :event_id)
+  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
 class Event < ActiveRecord::Base
   belongs_to :status
+  has_many :comments, -> { order(created_at: :desc) }
 end
