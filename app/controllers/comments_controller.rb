ad0b81b07bdc301bfd6d2fd3c15d806de00c2098
Kiedff <kiedff7@gmail.com>
2016-07-28 18:09:32 +0100

    adds ability to change event status when adding comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,14 +3,15 @@ class CommentsController < ApplicationController
   def new
     @event = Event.find(params[:event_id])
     @comment = Comment.new
+    @statuses = Status.all
   end
 
   def create
-    
     event = Event.find(params[:event_id])
+    event.status_id = params[:status_id]
     comment = event.comments.new(comment_params)
 
-    if comment.save
+    if comment.save && event.save
       flash[:notice] = "Comment successfully created"
     else
       flash[:error] = "Unable to create comment"
